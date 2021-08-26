f60d13460a59f24cf4116bc7f3e9305683de75fa
Raj Shah <rshah28@ncsu.edu>
2021-08-26 03:33:42 -0400

    improved css & added vote count, upvote, downvote (non-func)

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,16 +1,21 @@
 import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Product = () => {
   let { id } = useParams();
   let features = [
     {
-      "text": "Improve UI"
+      "text": "Improve UI",
+      "votes": 100
     },
     {
-      "text": "Add textbox to submit new feature"
+      "text": "Add textbox to submit new feature",
+      "votes": 50
     },
     {
-      "text": "Add upvote/downvote"
+      "text": "Add upvote/downvote",
+      "votes": 10
     }
   ]
   return (
@@ -18,11 +23,22 @@ const Product = () => {
       <div class="child">
         <h1>{id.toUpperCase()}</h1>
       </div>
-      {features.map((feature) =>
+      {features.sort((f1, f2) => f2.votes - f1.votes).map((feature) =>
         <>
-        <div class="child feature">
-          <p>{feature.text}</p>
-        </div>
+          <div class="child feature">
+            <p>
+              <span style={{ marginTop: "auto", marginBottom: "auto" }}>
+                {feature.text}
+              </span>
+              <span style={{ marginTop: "auto", marginBottom: "auto", marginLeft: "auto", marginRight: "1rem"}}>
+                {feature.votes}
+              </span>
+              <span class="container" style={{ marginRight: "0px"}}>
+                <FontAwesomeIcon icon={faChevronUp} size="lg" className="voteup" />
+                <FontAwesomeIcon icon={faChevronDown} size="lg" className="votedown" />
+              </span>
+            </p>
+          </div>
         </>
       )}
     </div>
