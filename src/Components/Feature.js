8e3962dff52d53458652f4f6d0b1d33e5abe354a
Raj Shah <rshah28@ncsu.edu>
2021-08-26 14:28:05 -0400

    Added input, css for highlight, fixed up/down vote

--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -3,27 +3,35 @@ import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Feature = ({ features, index, setFeatures }) => {
   const updateVotes = (votes) => {
-    const updatedFeature = {...features[index]};
-    updatedFeature.votes = votes;
-    setFeatures(features.map((feature)=>feature.id === features[index].id ? updatedFeature : feature));
+    const updatedFeature = { ...features[index] };
+    let change = votes;
+    if (updatedFeature.voted === change) {
+      updatedFeature.voted = 0;
+      updatedFeature.votes = updatedFeature.votes - votes;
+    } else if (updatedFeature.voted !== 0) {
+      updatedFeature.voted = change;
+      updatedFeature.votes = updatedFeature.votes + votes + votes;
+    } else {
+      updatedFeature.voted = change;
+      updatedFeature.votes = updatedFeature.votes + votes;
+    }
+    setFeatures(features.map((feature) => feature.id === features[index].id ? updatedFeature : feature));
   }
   return (
-    <>
-      <div className="child feature">
-        <p>
-          <span style={{ marginTop: "auto", marginBottom: "auto" }}>
-            {features[index].text}
-          </span>
-          <span style={{ marginTop: "auto", marginBottom: "auto", marginLeft: "auto", marginRight: "1rem" }}>
-            {features[index].votes}
-          </span>
-          <span className="container" style={{ marginRight: "0px" }}>
-            <FontAwesomeIcon icon={faChevronUp} size="lg" className="voteup" onClick={() => { updateVotes(features[index].votes + 1) }} />
-            <FontAwesomeIcon icon={faChevronDown} size="lg" className="votedown" onClick={() => { updateVotes(features[index].votes - 1) }} />
-          </span>
-        </p>
-      </div>
-    </>
+    <div className="child feature">
+      <p>
+        <span style={{ marginTop: "auto", marginBottom: "auto" }}>
+          {features[index].text}
+        </span>
+        <span style={{ marginTop: "auto", marginBottom: "auto", marginLeft: "auto", marginRight: "1rem" }}>
+          {features[index].votes}
+        </span>
+        <span className="container" style={{ marginRight: "0px" }}>
+          <FontAwesomeIcon icon={faChevronUp} size="lg" className={features[index].voted <= 0? "voteup" : "votedUp"} onClick={() => { updateVotes(1) }} />
+          <FontAwesomeIcon icon={faChevronDown} size="lg" className={features[index].voted >= 0? "votedown" : "votedDown"} onClick={() => { updateVotes(-1) }} />
+        </span>
+      </p>
+    </div>
   );
 }
 
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -4,27 +4,37 @@ import Feature from './Feature';
 
 const Product = () => {
   let { id } = useParams();
+  const [newFeature, setNewFeature] = useState('');
+  const handleNewFeatureChange = (event) => {
+    setNewFeature(event.target.value);
+  }
   const [features, setFeatures] = useState([
     {
       "id": 1,
       "text": "Improve UI",
-      "votes": 100
+      "votes": 100,
+      "voted": 0
     },
     {
       "id": 2,
       "text": "Add textbox to submit new feature",
-      "votes": 99
+      "votes": 100,
+      "voted": 0
     },
     {
       "id": 3,
       "text": "Add upvote/downvote",
-      "votes": 10
+      "votes": 10,
+      "voted": 0
     }
   ]);
   return (
     <div className="container">
       <div className="child">
         <h1>{id.toUpperCase()}</h1>
+      </div>
+      <div className="child inputContainer">
+        <input className="inputBar" value={newFeature} onChange={handleNewFeatureChange} placeholder="Enter a feature that you'd love to see"></input>
       </div>
       {features.sort((f1, f2) => f2.votes - f1.votes).map(
         (feature, index) => <Feature key={feature.id} features={features} index={index} setFeatures={setFeatures} />
