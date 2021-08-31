3eb48224680d8f563e2613af85bc82ec19053a74
Parth Kanakiya <smartparth13@gmail.com>
2021-08-31 14:19:20 -0400

    Update mongoconnect.py
    Added ppprint for json printing

--- api/mongoconnect.py
+++ api/mongoconnect.py
@@ -1,2 +1,6 @@
 import pymongo
+from pprint import pprint
 client = pymongo.MongoClient("mongodb://dbuser:<password>@cluster0-shard-00-00.5q2t1.mongodb.net:27017,cluster0-shard-00-01.5q2t1.mongodb.net:27017,cluster0-shard-00-02.5q2t1.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mmm09m-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.test
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
