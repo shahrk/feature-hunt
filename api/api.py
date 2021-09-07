00e51d04155fdcac0645d34d2c78c21aac706e87
Parth <smartparth12@gmail.com>
2021-09-06 22:46:59 -0400

    Added cors bug fix

--- api/api.py
+++ api/api.py
@@ -1,18 +1,19 @@
 import time
 from flask import Flask
 import pymongo
-import pprint
+from flask_cors import CORS
 
 from bson.json_util import dumps
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-@app.route('/projects')
+@app.route('/products')
 def get_projects():
-    client = pymongo.MongoClient("mongodb://abc:pqrs@cluster0-shard-00-00.5q2t1.mongodb.net:27017,cluster0-shard-00-01.5q2t1.mongodb.net:27017,cluster0-shard-00-02.5q2t1.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mmm09m-shard-0&authSource=admin&retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb://abcd:pqrs@cluster0-shard-00-00.5q2t1.mongodb.net:27017,cluster0-shard-00-01.5q2t1.mongodb.net:27017,cluster0-shard-00-02.5q2t1.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mmm09m-shard-0&authSource=admin&retryWrites=true&w=majority")
     db = client.DB1
     result = db.products.find()
     client.close()    
