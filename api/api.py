a8e62755b61eb37d4956cbd9931c3660b556c4a2
Nirav Patel <niravpatel29976@gmail.com>
2021-09-12 14:00:00 +0100

    Commenting old code

--- api/api.py
+++ api/api.py
@@ -19,26 +19,26 @@ CORS(app)
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-@app.route('/products', methods=['GET', 'POST'])
-def get_projects():
-    CONFIG = yaml.safe_load(open("config.yaml","r"))["DATABASE"]
-    uri = "mongodb://%s:%s@%s" % (CONFIG["USERNAME"], CONFIG["PASSWORD"], CONFIG["HOST"])
-    client = pymongo.MongoClient(uri,  tlsCAFile=certifi.where())
-    db = client.DB1
-    if request.method == 'GET':
-        result = db.products.find()
-    elif request.method == 'POST':
-        data = request.json
-        for id, feature in enumerate(data['features']):
-            feature['_id'] = ObjectId()
-        print(data)
-        if data is None or data == {}:
-            return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-        result = db.products.insert_one(data).inserted_id
-    client.close()
-    return dumps(result)
+#@app.route('/products', methods=['GET', 'POST'])
+#def get_projects():
+#    CONFIG = yaml.safe_load(open("config.yaml","r"))["DATABASE"]
+#    uri = "mongodb://%s:%s@%s" % (CONFIG["USERNAME"], CONFIG["PASSWORD"], CONFIG["HOST"])
+#    client = pymongo.MongoClient(uri,  tlsCAFile=certifi.where())
+#    db = client.DB1
+#    if request.method == 'GET':
+#        result = db.products.find()
+#    elif request.method == 'POST':
+#        data = request.json
+#        for id, feature in enumerate(data['features']):
+#            feature['_id'] = ObjectId()
+#        print(data)
+#        if data is None or data == {}:
+#            return Response(response=json.dumps({"Error": "Please provide connection information"}),
+#                        status=400,
+#                        mimetype='application/json')
+#        result = db.products.insert_one(data).inserted_id
+#    client.close()
+#    return dumps(result)
 @app.route('/<productname>', methods=['GET', 'POST'])    
 def get_feature(productname):
     CONFIG = yaml.safe_load(open("config.yaml","r"))["DATABASE"]
