73754913494d11d2a4a5e51fd2f72f591c8cf1ce
Raj Shah <rshah28@ncsu.edu>
2021-09-22 01:51:12 -0400

    updated failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/PRODUCTS/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders navbar', () => {
+  render(<App />);
+  const submitProject = screen.getByText(/Submit Project/i);
+  const roadmap = screen.getByText(/Roadmap/i);
+  const feedback = screen.getByText(/Feedback/i);
+  expect(submitProject).toBeInTheDocument();
+  expect(roadmap).toBeInTheDocument();
+  expect(feedback).toBeInTheDocument();
 });
