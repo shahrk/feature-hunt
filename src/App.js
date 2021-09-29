4333312dd054036b3340ae47ae1f975f500a9eae
Parth Kanakiya <smartparth13@gmail.com>
2021-09-28 20:55:42 -0400

    Added linting and Code Formatter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch, Route
-} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Product from './Components/Product';
 import Comments from './Components/Comments';
 import Header from './Components/Header';
 import Home from './Components/Home';
-import { useState } from 'react';
+import {useState} from 'react';
 
 function App() {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('');
   return (
     <Router>
       <Switch>
-        <Route path="/:id" children={<><Header setQuery={setQuery}/><Product query={query}/><br /><br /><Comments /></>} />
+        <Route
+          path="/:id"
+          children={
+            <>
+              <Header setQuery={setQuery} />
+              <Product query={query} />
+              <br />
+              <br />
+              <Comments />
+            </>
+          }
+        />
         <>
-          <Header setQuery={setQuery}/>
-          <Home query={query} setQuery={setQuery}/>
+          <Header setQuery={setQuery} />
+          <Home query={query} setQuery={setQuery} />
         </>
       </Switch>
     </Router>
--- src/Service.js
+++ src/Service.js
@@ -1,44 +1,47 @@
-const baseUrl = "http://localhost:5000/";
+const baseUrl = 'http://localhost:5000/';
 const requestOptionsBuilder = (method, body, headers) => {
-  let options =  {
+  let options = {
     method: method,
-    headers: { 'Content-Type': 'application/json' }
-  }
-  if (body)
-    options['body'] = JSON.stringify(body);
-  if (headers)
-    options.headers = headers;
+    headers: {'Content-Type': 'application/json'},
+  };
+  if (body) options['body'] = JSON.stringify(body);
+  if (headers) options.headers = headers;
   return options;
 };
 const get = async (path, params) => {
   path = sanitizePath(path);
-  if (params)
-    path += new URLSearchParams(params);
+  if (params) path += new URLSearchParams(params);
   const response = await fetch(baseUrl + path);
   const data = await response.json();
   if (response.status >= 400 && response.status < 600) {
     return Promise.reject(response);
   }
   return data;
-}
+};
 const post = async (path, body) => {
   path = sanitizePath(path);
-  const response = await fetch(baseUrl + path, requestOptionsBuilder('POST', body));
+  const response = await fetch(
+    baseUrl + path,
+    requestOptionsBuilder('POST', body)
+  );
   const data = await response.json();
   if (response.status >= 400 && response.status < 600) {
     return Promise.reject(response);
   }
   return data;
-}
+};
 const put = async (path, body) => {
   path = sanitizePath(path);
-  const response = await fetch(baseUrl + path, requestOptionsBuilder('PUT', body));
+  const response = await fetch(
+    baseUrl + path,
+    requestOptionsBuilder('PUT', body)
+  );
   const data = await response.json();
   if (response.status >= 400 && response.status < 600) {
     return Promise.reject(response);
   }
   return data;
-}
+};
 const remove = async (path) => {
   path = sanitizePath(path);
   const response = await fetch(baseUrl + path, requestOptionsBuilder('DELETE'));
@@ -47,7 +50,7 @@ const remove = async (path) => {
     return Promise.reject(response);
   }
   return data;
-}
-const sanitizePath = (p) => p.charAt && p.charAt(0) === '/' ? p.slice(1) : p;
+};
+const sanitizePath = (p) => (p.charAt && p.charAt(0) === '/' ? p.slice(1) : p);
 const Service = {get, post, put, delete: remove};
 export default Service;
