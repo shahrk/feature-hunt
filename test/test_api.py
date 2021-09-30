09942ac656f903a8448771daf0d9c371e99a9434
BhargavJethwa <70560970+BhargavJethwa@users.noreply.github.com>
2021-09-29 23:33:25 -0400

    delete old api

--- test/test_api.py
+++ test/test_api.py
@@ -1,42 +1,6 @@
-from flask import Flask
-import os
-import sys
-import mongomock
 from flask_cors import CORS
+import requests
 
-from flask_pymongo import PyMongo
-sys.path.insert(1, os.getcwd())
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-os.environ.update({'ROOT_PATH': ROOT_PATH})
-    
-
-import pytest
-
-from backend.index import app,PORT
-
-
-@pytest.fixture(autouse=True)
-def client():
-    print("here")
-    app.run()
-    with app.test_client() as client:
-        yield client
-
-# def test_product():
-
-#     rv = app.test_client().get('/products')
-#     print(rv.data)
-#     assert 200 in rv.data
-
-# def test_product():
-
-#     rv = app.test_client().get('/feature-hunt')
-#     print(rv.data)
-#     # assert 200 in rv.data
-
-def test_product(client):
-    print("here1")
-
-    rv = client.get('/feature-hunt/features')
-
-    # assert 200 in rv.data
+def test_products():
+    response = requests.get("http://localhost:5000/products")
+    print(response)        
