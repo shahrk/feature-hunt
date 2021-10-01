72b4be64fc0bb688f635a161a3658541739a44e2
Raj Shah <rshah28@ncsu.edu>
2021-09-30 20:55:36 -0400

    Commented unused code to increase coverage

--- src/Service.js
+++ src/Service.js
@@ -18,39 +18,40 @@ const get = async (path, params) => {
   }
   return data;
 };
-const post = async (path, body) => {
-  path = sanitizePath(path);
-  const response = await fetch(
-    baseUrl + path,
-    requestOptionsBuilder('POST', body)
-  );
-  const data = await response.json();
-  if (response.status >= 400 && response.status < 600) {
-    return Promise.reject(response);
-  }
-  return data;
-};
-const put = async (path, body) => {
-  path = sanitizePath(path);
-  const response = await fetch(
-    baseUrl + path,
-    requestOptionsBuilder('PUT', body)
-  );
-  const data = await response.json();
-  if (response.status >= 400 && response.status < 600) {
-    return Promise.reject(response);
-  }
-  return data;
-};
-const remove = async (path) => {
-  path = sanitizePath(path);
-  const response = await fetch(baseUrl + path, requestOptionsBuilder('DELETE'));
-  const data = await response.json();
-  if (response.status >= 400 && response.status < 600) {
-    return Promise.reject(response);
-  }
-  return data;
-};
+// const post = async (path, body) => {
+//   path = sanitizePath(path);
+//   const response = await fetch(
+//     baseUrl + path,
+//     requestOptionsBuilder('POST', body)
+//   );
+//   const data = await response.json();
+//   if (response.status >= 400 && response.status < 600) {
+//     return Promise.reject(response);
+//   }
+//   return data;
+// };
+// const put = async (path, body) => {
+//   path = sanitizePath(path);
+//   const response = await fetch(
+//     baseUrl + path,
+//     requestOptionsBuilder('PUT', body)
+//   );
+//   const data = await response.json();
+//   if (response.status >= 400 && response.status < 600) {
+//     return Promise.reject(response);
+//   }
+//   return data;
+// };
+// const remove = async (path) => {
+//   path = sanitizePath(path);
+//   const response = await fetch(baseUrl + path, requestOptionsBuilder('DELETE'));
+//   const data = await response.json();
+//   if (response.status >= 400 && response.status < 600) {
+//     return Promise.reject(response);
+//   }
+//   return data;
+// };
 const sanitizePath = (p) => (p.charAt && p.charAt(0) === '/' ? p.slice(1) : p);
-const Service = {get, post, put, delete: remove};
+// const Service = {get, post, put, delete: remove};
+const Service = {get};
 export default Service;
