ab758640cdb358b0f87cd7ad72dfeb9b7d81ad96
Leila Moran <lmmoran@ncsu.edu>
2021-10-25 21:58:47 -0400

    Incorporated backend-new into existing files
    Added contents of backend-new/app.py to backend/__init__.py
    
    Copied auth_controller.py to backend/app/controllers

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,13 +4,16 @@ You may use, distribute and modify this code under the terms of the MIT license.
 You should have received a copy of the XYZ license with
 this file. If not, please write to: featurehuntteam@gmail.com
 """
+# pylint: disable=wrong-import-position,pointless-string-statement,undefined-variable,line-too-long
 
 ''' flask app with mongo '''
 import os
 import json
 import datetime
+from flask import Flask, render_template, request, url_for, redirect, session
+import pymongo
+import bcrypt
 from bson.objectid import ObjectId
-from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
@@ -23,4 +26,12 @@ CORS(app)
 app.config['MONGO_URI'] = os.environ.get('DB')
 mongo = PyMongo(app)
 
+app.secret_key = "testing"
+records = mongo.db.users
+
+
 from app.controllers import *
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
