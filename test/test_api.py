20120821a078a9e10540d7ebafe7afc284bddda5
Leila Moran <lmmoran@ncsu.edu>
2021-11-02 18:56:57 -0400

    Commented out unused tests, added pylint comments
    * Restored and commented out unused tests
    * Added pylint skip comments

--- test/test_api.py
+++ test/test_api.py
@@ -1,7 +1,9 @@
-''' docstr '''
+#pylint: skip-file
 from pprint import pprint
 from flask_cors import CORS
 import requests
 
 def test_products():
+    #response = requests.get("http://localhost:5000/products") 
+    #assert b'feature-hunt' in response.content
     pass
